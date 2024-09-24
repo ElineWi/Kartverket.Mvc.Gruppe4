a9889ccf90a56265fe7810b424019dfbae16fe3c
ElineWi <elinewi@uia.no>
2024-09-24 17:46:29 +0200

    Endret på BrukerController

--- Kartverket.Test/Controllers/BrukerController.cs
+++ Kartverket.Test/Controllers/BrukerController.cs
@@ -1,6 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+
 namespace Kartverket.Test.Controllers;
 
-public class BrukerController
+public class BrukerController : Controller
 {
-    
+// GET
+    public IActionResult Index()
+    {
+        return View();
+    }
 }
--- Kartverket.Test/obj/Debug/net8.0/Kartverket.Test.AssemblyInfo.cs
+++ Kartverket.Test/obj/Debug/net8.0/Kartverket.Test.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Kartverket.Test")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1e8cb99723a859b918b6e372c016836efc552041")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f2e1014e122c01126df331fe6958292a2705bb0c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Kartverket.Test")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Kartverket.Test")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
